82756495c4a61a2be77f4306ae903f7d47f48865
Florian Kuc <florian.kuc@gmail.com>
2021-09-15 17:53:28 +0200

    refactor: _map

--- src/fns/_map.ts
+++ src/fns/_map.ts
@@ -1,15 +1,9 @@
 export type T_map = (value: any, cb: Function) => void;
 
-const mockObject = {
-  id: '1',
-  data: 'value',
-};
-
 export const _map: T_map = (value, cb) => {
   if (!Array.isArray(value)) {
     value = [value];
   }
 
-  // return value.map((v: typeof value, i: number, src: typeof value[]) => (x: any) => cb(x));
   return value.map(cb);
 };
